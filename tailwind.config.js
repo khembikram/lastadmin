31cb3ce9398b3e2812d2d1f6b2e27dcfe2ccdac5
Sujan Rana <Khembikram1@gmail.com>
2024-01-21 16:04:00 +0545

    main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,51 +1,36 @@
-
 module.exports = {
     content: [
-        ".{html,js}",
+        "./.{html,js}",
         'node_modules/preline/dist/*.js',
     ],
     darkMode: ['class', '[data-mode="dark"]'],
     theme: {
-
         container: {
             center: true,
         },
-
         fontFamily: {
             'base': ['Noto Sans', 'sans-serif'],
         },
-
         extend: {
             colors: {
                 'primary': '#458bc4',
-
                 'secondary': '#6c757d',
-
                 'success': '#4fc55b',
-
                 'warning': '#e2ab3b',
-
                 'info': '#3db9dc',
-
                 'danger': '#d57171',
-
                 'purple': '#aa81f3',
-
                 'pink': '#ff8acc',
-
                 'teal': '#23b195',
-
                 'light': '#f5f5f5',
-
                 'dark': '#626773',
             },
         },
     },
-
     plugins: [
         require('preline/plugin'),
         require('@tailwindcss/forms'),
         // require('@tailwindcss/aspect-ratio'),
         // require('@tailwindcss/typography'),
     ],
-}
+};
